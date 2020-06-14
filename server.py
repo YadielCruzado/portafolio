d4264538abf1b28bd9930bdbce2ac3d93d179fb8
YadielCruzado <yadiel.cruzado@upr.edu>
2020-06-14 14:42:22 -0400

    my portafolio

---
+++ server.py
@@ -0,0 +1,40 @@
+# set FLASK_APP=server.py
+# set FLASK_ENV=development
+# flask run
+
+from flask import Flask, render_template, request
+import csv
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/<string:page_name>')
+def html_page(page_name):
+    return render_template(page_name)
+
+
+def write_to_csv(data):
+    with open('database.csv', newline='', mode='a') as database2:
+        email = data["email"]
+        name = data["name"]
+        message = data["message"]
+        csv_writer = csv.writer(database2, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow([name, email, message])
+
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    if request.method == 'POST':
+        try:
+            data = request.form.to_dict()
+            write_to_csv(data)
+            return 'form submitted'
+        except:
+            return 'did not saves'
+    else:
+        return 'ups'
